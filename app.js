032767d270f0e0cc162dfcdbba4e56e6086f638b
huzzaifa-khan-Github-Account <hk896402@gmail.com>
2024-07-03 01:01:16 +0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let personname = '';
-personname = prompt('what is your name?') || '';
-if (personname !== null && personname !== '') {
-    alert(`Hello ${personname}, would you like to learn some Python today?`);
-}
-else {
-    alert('you have to fill your name');
-}
